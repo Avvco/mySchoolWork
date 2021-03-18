579a241d5cc2b7a0e5c98a0c93ad96578fc508e9
Avvco <leei910110@gmail.com>
2021-03-18 21:24:36 +0800

    save

--- S2/W4/q16.cpp
+++ S2/W4/q16.cpp
@@ -87,19 +87,71 @@ class Segment {
     int getC() {
       return c;
     }
+    Point getpt1() {
+      return point1;
+    }
 
+    Point getpt2() {
+      return point2;
+    }
 
     float getLength() {
       return point1.mypow(point1.mypow((point1.getX() - point2.getX()), 2) + point1.mypow((point1.getY() - point2.getY()), 2), 0.5);
     }
 
     Point *getIntersect(Segment s) {
-      double d = this -> a * s.getB() - s.getA() * this -> b;
-      if(d == 0) return NULL;
       Point * pt = new Point();
-      pt -> setX((this -> b * s.getC() - s.getB() * this -> c) / d);
-      pt -> setY((this -> c * s.getA() - s.getC() * this -> a) / d);
-      return pt;
+      // start pt or end pt connected
+      if(connected(point1, s.getpt1())) {
+        pt -> setX(point1.getX());
+        pt -> setY(point1.getY());
+        return pt;
+      }else if(connected(point1, s.getpt2())) {
+        pt -> setX(point1.getX());
+        pt -> setY(point1.getY());
+        return pt;
+      }else if(connected(point2, s.getpt1())) {
+        pt -> setX(point2.getX());
+        pt -> setY(point2.getY());
+        return pt;
+      }else if(connected(point2, s.getpt2())) {
+        pt -> setX(point2.getX());
+        pt -> setY(point2.getY());
+        return pt;
+      }
+
+      double d = this -> a * s.getB() - s.getA() * this -> b;
+      std::cout << d << " ";
+      std::cout << this -> a << " " << s.getB() << " " << s.getA() << " " << this -> b;
+      if(a == 0 && d == 0) {
+        if(inside(point1.getY(), point2.getY(), s.getpt1().getY())) {
+          pt -> setX(point1.getX());
+          pt -> setY((((-1) * s.getA() * point1.getX() - s.getC())) / s.getB());
+          return pt;
+        }
+      }
+
+      if(s.getA() == 0 && d == 0) {
+        if(inside(s.getpt1().getY(), s.getpt2().getY(), point1.getY())) {
+          pt -> setX(s.getpt1().getX());
+          pt -> setY(((-1) * a * s.getpt1().getX() - c) / b);
+          return pt;
+        }
+      }
+
+
+
+      if(d == 0) return NULL;
+      float interX = (this -> b * s.getC() - s.getB() * this -> c) / d;
+      float interY = (this -> c * s.getA() - s.getC() * this -> a) / d;
+
+      if(inside(this -> point1.getX(), this -> point2.getX(), interX) && inside(this -> point1.getY(), this -> point2.getY(), interY)
+        && inside(s.getpt1().getX(), s.getpt2().getX(), interX) && inside(s.getpt1().getY(), s.getpt2().getY(), interY)) {
+        pt -> setX(interX);
+        pt -> setY(interY);
+        return pt;
+      }
+      return NULL;
     }
 
   private:
@@ -118,13 +170,27 @@ class Segment {
     void calC() {
       this -> c =  point1.getX() * point2.getY() - point2.getX() * point1.getY();
     }
+    bool inside(float x1, float x2, float p) {
+      if(x1 >= p && p >= x2) return true;
+      if(x2 >= p && p >= x1) return true;
+      return false;
+    }
+    bool connected(Point p1, Point p2) {
+      if(p1.getX() == p2.getX() && p1.getY() == p2.getY()) return true;
+      return false;
+    }
 };
 
 int main(int argc, char const *argv[]) {
-  Point p1(1.6, 7.9);
-  Point p2(5.7,8.4);
-  Segment s(p1,p2);
-  std::cout<<s.getLength();
+  Point p1(0, 0);
+  Point p2(2,0);
+  Point p3(1, 1);
+  Point p4(1, -1);
+  Segment s1(p1,p2);
+  Segment s2(p3,p4);
+  Point * pt1 = s1.getIntersect(s2);
+  std::cout << "XXXXX ";
+  std::cout<<pt1 -> getX() << " " << pt1 -> getY();
 
   return 0;
 }
