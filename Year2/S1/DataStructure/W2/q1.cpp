89138af0bd787eb7a41bcfb9fc51a2ce825dd9e0
Avvco <leei910110@gmail.com>
2021-10-03 16:37:12 +0800

    DSW2 update

--- Year2/S1/DataStructure/W2/q1.cpp
+++ Year2/S1/DataStructure/W2/q1.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 void allocArray(int ***p, int m, int n)
 {
-  int** ary = new int*[m];
-  for(int i = 0; i < m; i++) {
-    ary[i] = new int [n];
+  int *_p = new int[m*n];
+  *p = new int*[m];
+  for(int i=0; i<m; i++) {
+    (*p)[i] = &_p[i*n];
   }
-  *p = ary;
 }
 
 int main()
@@ -18,8 +18,8 @@ int main()
   int j, k;
   allocArray(&array, 5, 10);
 
-  // int*** prev = &array;
-  // int counter = 0;
+  int*** prev = &array;
+  int counter = 0;
   for(j = 0;j < 5;j ++)
     for(k = 0;k < 10;k ++) {
       array[j][k] = j * 10 + k;
@@ -28,11 +28,11 @@ int main()
 
   for(j = 0;j < 5;j ++) {
     for(k = 0;k < 10;k ++) {
-      printf("%p ", &(array[j][k]));
+      //printf("%p ", &(array[j][k]));
 
-      // printf("%d %d %d\n", counter, int(&(array[j][k])) , int(**prev));
-      // **prev = &(array[j][k]);
-      // counter ++;
+      printf("%d %d \n", counter, int(&(array[j][k])) - int(**prev));
+      **prev = &(array[j][k]);
+      counter ++;
     }
   }
 }
