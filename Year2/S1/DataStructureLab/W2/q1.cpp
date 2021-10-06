80bc0854c08c63877f5b089485bbcf6514d99a1a
Avvco <leei910110@gmail.com>
2021-10-06 20:31:56 +0800

    Y2S1DSW2 fix

--- Year2/S1/DataStructureLab/W2/q1.cpp
+++ Year2/S1/DataStructureLab/W2/q1.cpp
@@ -18,21 +18,20 @@ int main()
   int j, k;
   allocArray(&array, 5, 10);
 
-  int*** prev = &array;
-  int counter = 0;
+  //int*** prev = &array;
+  //int counter = 0;
   for(j = 0;j < 5;j ++)
     for(k = 0;k < 10;k ++) {
       array[j][k] = j * 10 + k;
-      cout << &array[j][k] << " ";
+      // cout << &array[j][k] << " ";
     }
 
   for(j = 0;j < 5;j ++) {
     for(k = 0;k < 10;k ++) {
-      //printf("%p ", &(array[j][k]));
-
-      printf("%d %d \n", counter, int(&(array[j][k])) - int(**prev));
-      **prev = &(array[j][k]);
-      counter ++;
+      printf("%p ", &(array[j][k]));
+      //printf("%d %d \n", counter, int(&(array[j][k])) - int(**prev));
+      //**prev = &(array[j][k]);
+      //counter ++;
     }
   }
 }
