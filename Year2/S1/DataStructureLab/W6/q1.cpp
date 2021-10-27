b3ac4fc9e82664185518e5ef35103da03225cc7e
Avvco <leei910110@gmail.com>
2021-10-27 21:32:55 +0800

    Y2S1DSW6 save

---
+++ Year2/S1/DataStructureLab/W6/q1.cpp
@@ -0,0 +1,188 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+
+#define SIZE 10
+
+using namespace std;
+
+class Node {
+  public:
+  	Node(): Node(0, 0) {}
+  	Node(int r, int c) {
+  		row = r;
+  		col = c;
+  	}
+  	int getRow() const { return row; }
+  	int getCol() const { return col; }
+  	void setRow(int r) {
+  		if(r >= 0 && r < SIZE) {
+        row = r;
+      }
+  	}
+  	void setCol(int c) {
+  		if(c >= 0 && c < SIZE) {
+        col = c;
+      }
+  	}
+  private:
+  	int col, row;
+};
+
+class List {
+  public:
+  	List() {
+  		top = 0;
+  	}
+  	/*
+  	function addElement
+  	Insert an element into list
+  	*/
+  	void addElement(int r, int c) {
+      //cout<< r << " "<<c<<endl;
+      data[top] = Node(r, c);
+      top++;
+  	}
+  	/*
+  	function removeElement
+  	remove an element from list and return a pointer point to the element.
+  	If list is empty, return NULL.
+  	*/
+  	Node *removeElement() {
+      if(top == 0) return NULL;
+      top--;
+      return &data[top + 1];
+  	}
+  	void printList() {
+  		int j;
+      if(top == 0) {
+        cout<<"NULL";
+      }
+  		for(j = top;j >= 0;j--) {
+  			cout<<"("<<data[j].getRow()<<", "<<data[j].getCol()<<")"<<endl;
+  		}
+  	}
+    int getTop() {
+      return top;
+    }
+  private:
+  	Node data[SIZE * SIZE];
+  	int top;
+};
+
+class Maze {
+  public:
+  	Maze() {
+  		initMaze(SIZE);
+  	}
+  	/*
+  	function initMaze
+  	Alocate a 2-D array with s * s sizes as the map of maze.
+  	Inside the maze where 0 represent empty space and 1 represent wall.
+  	[0][0] is start point and [s - 1][s - 1] is finish point.
+  	Randomly generate 20% wall in the maze.
+  	Make sure [0][0] and [s - 1][s - 1] are 0
+
+  	動態配置一個二維的陣列，大小是 s * s ，用這個陣列表示迷宮的地圖
+  	陣列中 0 的值表示可以移動的空間， 1 表示牆壁
+  	[0][0] 的位置表示起點 [s - 1][s - 1] 的位置表示終點
+  	請在迷宮中加入 20% 的牆壁
+  	然後確定起點跟終點都是可以移動的位置
+  	*/
+  	void initMaze(int s) {
+      int **_maze = new int*[s];
+      for (int i = 0; i < s; ++i) {
+        _maze[i] = new int[s];
+      }
+      for (int i = 0; i < s; i++) {
+        for (int j = 0; j < s; j++) {
+          int random = rand() % 100; // 20/100
+
+          if(random < 25) {
+            _maze[i][j] = 1; // wall
+          } else {
+            _maze[i][j] = 0; // road
+          }
+          if((i == 0 && j == 0) || (i == s - 1 && j == s-1)) {
+            _maze[i][j] = 0;
+          }
+
+          cout<<_maze[i][j]<< " ";
+        }
+        cout<<endl;
+      }
+      maze = _maze;
+  	}
+  	/*
+  	function getPath
+  	This function will find a path between start point and finish point.
+  	Return a list content the path information inside.
+  	If there is no path between two point then the list will be empty.
+
+  	這個函數會找到起點到終點間的一條路徑
+  	回傳一個 list， List 包含路徑的資訊
+  	如果找不到路徑的話 list 就會是空的
+  	*/
+  	List *getPath() {
+      List *list = new List();
+
+      visit(list, 0, 0);
+      if(list -> getTop() == 0) return NULL;
+      return list;
+  	}
+  	void printMaze() {
+  		int j, k;
+
+  		for(j = 0;j < SIZE;j ++) {
+  			for(k = 0;k < SIZE;k ++) {
+  				if(maze[j][k] == 0) {
+            cout<<" ";
+          }
+          else if(maze[j][k] == 1) {
+            cout<<"*";
+          }
+
+  			}
+  			cout<<"\n";
+  		}
+  	}
+
+    void visit(List *list, int x, int y) {
+      //cout<< x << " "<<y<<endl;
+      if(x == SIZE - 1 && y == SIZE - 1) {
+        list->addElement(SIZE-1, SIZE-1);
+        reach = true;
+        return;
+      }
+      maze[x][y] = 3;
+    // =========
+      if(!reach && (y + 1) < SIZE && maze[x][y+1] == 0) {
+        visit(list, x, y+1);
+      }
+      if(!reach && (y - 1) >= 0 && maze[x][y-1] == 0) {
+        visit(list, x, y-1);
+      }
+      if(!reach && (x + 1) < SIZE && maze[x+1][y] == 0) {
+        visit(list, x+1, y);
+      }
+      if(!reach && (x - 1) >= 0 && maze[x-1][y] == 0) {
+        visit(list, x-1, y);
+      }
+      if(maze[x][y] == 3) maze[x][y] = 0;
+    // ========
+      if(reach) {
+        //cout<< x << " "<<y<<endl;
+        list -> addElement(x, y);
+      }
+    }
+  private:
+  	int **maze;
+    bool reach = false;
+};
+
+int main() {
+  srand(time(NULL));
+	Maze *maze = new Maze();
+	maze->printMaze();
+	maze->getPath()->printList();
+}
---
+++ Year2/S1/DataStructureLab/W6/q2.cpp
@@ -0,0 +1,195 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+
+using namespace std;
+
+#define SIZE 10
+#define UP 0
+#define DOWN 1
+#define LEFT 2
+#define RIGHT 3
+
+class Grid {
+  public:
+  	Grid():Grid(0) {}
+  	Grid(int s) {
+  		state = s;
+  		dir[UP] = NULL;
+  		dir[DOWN] = NULL;
+  		dir[LEFT] = NULL;
+  		dir[RIGHT] = NULL;
+  	}
+  	Grid *getDir(int d) const { return dir[d]; }
+  	int getState() const { return state; }
+  	void setDir(int d, Grid *g) { dir[d] = g; }
+  	void setState(int s) { state = s;}
+  private:
+  	Grid *dir[4];
+  	int state;
+};
+
+class List {
+  public:
+  	List() {
+  		top = 0;
+  	}
+  	/*
+  	Insert an element from top
+  	*/
+  	void addElement(Grid *g){
+			data[top] = g;
+      top++;
+  	}
+  	/*
+  	remove an element from top and return a pointer point to the element.
+  	If list is empty, return NULL.
+  	*/
+  	Grid *removeElement() {
+			if(top == 0) return NULL;
+			top--;
+			return data[top+1];
+  	}
+  	void printPath() const {
+  		int j;
+  		for(j = top ; j >= 0 ; j--) {
+  			if(data[j] == data[j - 1]->getDir(UP)) {
+  				cout<<"UP\n";
+  			}
+  			else if(data[j] == data[j - 1]->getDir(DOWN)) {
+  				cout<<"DOWN\n";
+  			}
+  			else if(data[j] == data[j - 1]->getDir(LEFT)) {
+  				cout<<"LEFT\n";
+  			}
+  			else if(data[j] == data[j - 1]->getDir(RIGHT)) {
+  				cout<<"RIGHT\n";
+  			}
+  		}
+  	}
+		int getTop() {
+			return top;
+		}
+  private:
+  	Grid *data[SIZE * SIZE];
+  	int top;
+};
+
+class Maze {
+  public:
+  	Maze() {
+  		initMaze(SIZE);
+  	}
+  	/*
+  	function initMaze
+  	Alocate a 2-D link list with s * s sizes as the map of maze.
+  	Inside the maze enery gird with state 0 represent empty space and 1 represent wall.
+  	The gird in left top is start point and right bottom is finish point.
+  	Randomly generate 20% wall in the maze.
+  	Make sure start point and finish point are 0
+
+  	動態配置一個二維的鏈結串列，大小是 s * s，用這個鏈結串列表示迷宮的地圖
+  	節點中 0 的值表示可以移動的空間， 1 表示牆壁
+  	左上角的位置表示起點，右下角的位置表示終點
+  	請在迷宮中加入 20% 的牆壁
+  	然後確定起點跟終點都是可以移動的位置
+  	*/
+  	void initMaze(int s) {
+      Grid *_maze[s][s];
+			for (int i = 0; i < s; i++) {
+				for (int j = 0; j < s; j++) {
+					_maze[i][j] = new Grid();
+				}
+			}
+      for (int y = 0; y < s; y++) {
+        for (int x = 0; x < s; x++) {
+          int random = rand() % 100; // 20/100
+
+					// WALL
+          if(random < 25) {
+            _maze[y][x]->setState(1); // wall
+          } else {
+            _maze[y][x]->setState(0); // road
+          }
+          if((y == 0 && x == 0) || (y == s - 1 && x == s-1)) {
+            _maze[y][x]->setState(0);
+          }
+					// =========
+					// Diraction
+						if((y + 1) < SIZE) _maze[y][x]->setDir(DOWN, _maze[y + 1][x]);
+						if((y - 1) >= 0) _maze[y][x]->setDir(UP, _maze[y - 1][x]);
+						if((x + 1) < SIZE) _maze[y][x]->setDir(RIGHT, _maze[y][x + 1]);
+						if((x - 1) >= 0) _maze[y][x]->setDir(LEFT, _maze[y][x - 1]);
+					// =========
+          //cout <<_maze[y][x]->getState()<< " ";
+        }
+        //cout<<endl;
+      }
+      maze = **_maze;
+  	}
+  	/*
+  	function getPath
+  	This function will find a path between start point and finish point.
+  	Return a list content the path information inside.
+  	If there is no path between two point then the list will be empty.
+
+  	這個函數會找到起點到終點間的一條路徑
+  	回傳一個 list 包含著路徑的資訊
+  	如果找不到路徑的話 list 就會是空的
+  	*/
+  	List *getPath() {
+			List *list = new List();
+      visit(list, maze);
+      if(list -> getTop() == 0) return NULL;
+      return list;
+  	}
+  	void printMaze() const {
+  		Grid *j = maze, *k;
+  		while(j != NULL) {
+  			k = j;
+  			while(k != NULL) {
+  				cout<<k->getState()<<" ";
+  				k = k->getDir(RIGHT);
+  			}
+  			cout<<endl;
+  			j = j->getDir(DOWN);
+
+  		}
+  	}
+
+		void visit(List *list, Grid* maze) {
+      if(maze->getDir(RIGHT) == NULL && maze->getDir(DOWN)) {
+        list->addElement(maze);
+        reach = true;
+        return;
+      }
+      maze -> setState(3);
+    // =========
+      if(!reach && maze->getDir(UP) && maze->getDir(UP)->getState() == 0) {
+        visit(list, maze->getDir(UP));
+      }
+      if(!reach && maze->getDir(DOWN) && maze->getDir(DOWN)->getState() == 0) {
+        visit(list, maze->getDir(DOWN));
+      }
+      if(!reach && maze->getDir(LEFT) && maze->getDir(LEFT)->getState() == 0) {
+        visit(list, maze->getDir(LEFT));
+      }
+      if(!reach && maze->getDir(RIGHT) && maze->getDir(RIGHT)->getState() == 0) {
+        visit(list, maze->getDir(RIGHT));
+      }
+      if(maze->getState() == 3) maze->setState(0);
+    // ========
+      if(reach) {
+        list -> addElement(maze);
+      }
+    }
+  private:
+  	Grid *maze;
+		bool reach;
+};
+
+int main() {
+	Maze *maze = new Maze();
+	maze->printMaze();
+	maze->getPath()->printPath();
+}
