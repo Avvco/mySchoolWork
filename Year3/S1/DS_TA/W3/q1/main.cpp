33461d1934554666fe313b4580ea38476b17d793
Avvco <leei910110@gmail.com>
2022-07-08 02:46:45 +0800

    Y3S1DSTAW3Q1 save

---
+++ Year3/S1/DS_TA/W3/q1/main.cpp
@@ -0,0 +1,33 @@
+/**
+ * @Description:
+ *  Please write a program that reads in an expression and gives its result.
+ *  The expression will contain addition, subtraction, multiplication, division, power, and parentheses.
+ *  Please follow PEMDAS rules and round to the second decimal place after every operation.
+ *  You must use the class "Stack" you wrote last week or you will get up to 0 points.
+ * 
+ * Rounding rule:
+ * Banker's rounding
+
+ * Symbol example:
+ * addition: "+"
+ * subtraction: "-"
+ * multiplication: "*"
+ * division: "/"
+ * power: "^"
+ * parentheses: "()"
+ * 
+ * @Input Description
+ * Expressions end up with line-break or end-of-file.
+ * 
+ * @Output Description
+ * Result of the expression, "ERROR" when parentheses count does not match or operation error, ending with a "\n" of each line
+ * 
+ */ 
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+}
