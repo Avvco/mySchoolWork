08c056fe2d5c0c001d22afbe30c01f1f48a53307
Avvco <leei910110@gmail.com>
2022-07-08 22:48:28 +0800

    Y3S1DSTAW3Q2 save

---
+++ Year3/S1/DS_TA/W3/q2/main.cpp
@@ -0,0 +1,25 @@
+/**
+ * Tower of Hanoi
+ * 
+ * @Description
+ * Tower of Hanoi is a mathematical puzzle where we have three rods and n disks. The objective of the puzzle is to move the entire stack to another rod, obeying the following simple rules:
+ * 1. Only one disk can be moved at a time.
+ * 2. Each move consists of taking the upper disk from one of the stacks and placing it on top of another stack i.e. a disk can only be moved if it is the uppermost disk on a stack.
+ * 3. No disk may be placed on top of a smaller disk.
+ * 
+ * There are only 3 rods "A", "B" and "C", you need to move all the disks from rod "A" to rod "C" and print the minimum number of moves required to complete the question.
+ * 
+ * Note: You must use your own "Stack" class, no recursive function can be used and no formula like 2^N-1 can be used, or you will get up to 0 points.
+ * 
+ * @Input Description
+ * The number of the disks on the rod "A", ends up with line-break or end-of-file.
+ * 
+ * @Output Description
+ * The minimum number of moves required, ending with a "\n" of each line
+ */
+
+#include <iostream>
+
+int main() {
+  return 0;
+}
