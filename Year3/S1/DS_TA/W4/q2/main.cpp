7bb94f2ac2a7310614e8a9ad130540093b7614c3
Avvco <leei910110@gmail.com>
2022-07-12 21:14:05 +0800

    Y3S1DSTAW4 save

---
+++ Year3/S1/DS_TA/W4/q2/main.cpp
@@ -0,0 +1,19 @@
+/**
+ * Arithmetic II
+ * 
+ * @Description
+ * Please use the class "BigDecimal" you wrote to redo the Arithmetic problem you did last week.
+ * All the rules are the same as the Arithmetic problem in the last week.
+ * 
+ * @Input Description
+ * Expressions end up with line-break or end-of-file.
+ * 
+ * @Output Description
+ * Result of the expression to the 2nd decimal place, "ERROR" when parentheses count does not match or operation error, ending with a "\n" of each line
+ */
+
+#include <iostream>
+
+int main() {
+  return 0;
+}
