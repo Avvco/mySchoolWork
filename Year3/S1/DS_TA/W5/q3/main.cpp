4bb40dbe71400ee8051f0294cf52e346197190ff
Avvco <leei910110@gmail.com>
2022-08-17 22:33:54 +0800

    Y3S1DSTAW5Q3 save

---
+++ Year3/S1/DS_TA/W5/q3/main.cpp
@@ -0,0 +1,27 @@
+/**
+ * Expression Tree
+ * 
+ * @Description:
+ * Please replace the class "Stack" with the class "BinarySearchTree" to redo the question "Arithmetic II".
+ * All the rules are the same as the Arithmetic problem, except there are no parentheses.
+ * Serialize and deserialize rules are the same as the question "Binary Search Tree II".
+ * You need to deserialize the string with levelorder traversal and then read the expression tree with inorder traversal.
+ * You can add any function or variable if you want.
+ * You must use your own class "BinarySearchTree" you wrote or you will get up to 0 points.
+ * 
+ * @Input Description
+ * A serialized string that represents the expression tree.
+ *  
+ * 
+ * @Output Description
+ * Result of the expression tree to the 2nd decimal place, "ERROR" when operation error, ending with a "\n" of each line
+ */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main() {
+  return 0;
+}
