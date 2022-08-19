e4d8dc339f96190095e87dcc80c02a3fe7917968
Avvco <leei910110@gmail.com>
2022-08-19 19:59:29 +0800

    Y3S1DSTAW6Q2 save

---
+++ Year3/S1/DS_TA/W6/q2/main.cpp
@@ -0,0 +1,63 @@
+/**
+ * File System
+ * 
+ * @Description:
+ * Please use the class "GeneralTree" to simulate a Unix File System.
+ * Reference: https://www.geeksforgeeks.org/unix-file-system/
+ * But only Ordinary files and Directories will appear in the File System, which is determined by the tag "type" which is "dir" or "file"
+ * 
+ * There are five operations you need to do:
+ * 1. mv <source> <destination>
+ *  - move the file or directory from <source> to under <destination>
+ *  - Replace the files of <destination> if it exists
+ *  - Cancel operation if <source> does not exist.
+ * 2. cp <source> <destination>
+ *  - copy the file or directory from <source> to under <destination>
+ *  - Replace the files of <destination> if it exists
+ *  - Cancel operation if <source> does not exist.
+ * 3. rm <destination>
+ *  - remove the file or directory from <destination>
+ *  - Cancel operation if <destination> does not exist.
+ * 4. touch <destination> <data>
+ *  - add a file to <destination> that contains <data>
+ *  - Create directories which in the path if it does not exist.
+ *  - Replace the files of <destination> if it exists.
+ * 5. mkdir <destination>
+ *  - add a directory to <destination>
+ *  - Create directories which in the path if it does not exist.
+ *  - Cancel operation if <destination> already exist.
+ * <source> and <destination> are both absolute pathnames.
+ * <data> is a string, could be empty.
+ * 
+ * Serilized property of node for File:
+ * {
+ *  "data":"<data for the file>",
+ *  "path":"<relative pathnames for this node>",
+ *  "type":"file"
+ * }
+ * 
+ * Serilized property of node for Directory:
+ * {
+ *  "children":[<children of this node>],
+ *  "path":"<relative pathnames for this node>",
+ *  "type":"dir"
+ * }
+ * 
+ * You can add any function or variable if you want.
+ * Note: You must use your own "GeneralTree" class or you will get up to 0 points.
+ * 
+ * @Input Description
+ * The first line of input is the serilized string of the GeneralTree.
+ * The following line til the end will be the operations, one operation per line.
+ * 
+ * @Output Description
+ * A serizlied string of the GeneralTree after operations with a "\n" .
+ */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  return 0;
+}
