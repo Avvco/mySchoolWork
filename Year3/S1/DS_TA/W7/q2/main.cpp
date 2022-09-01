d2ca0c9fd65c9c872d9ac409f145f65c6a27914d
Avvco <leei910110@gmail.com>
2022-09-01 16:57:01 +0800

    Y3S1DSTAW7 save

---
+++ Year3/S1/DS_TA/W7/q2/main.cpp
@@ -0,0 +1,36 @@
+/**
+ * AVL Tree
+ * 
+ * @Description:
+ * Please finish the class "AVL_Tree" and main function to fulfill the requirements.
+ * You can choose to implement your own AVL Tree or inherit from your "BinarySearchTree" or "binarySearchTree".
+ * You can add any function or variable if you want.
+ * Serialize and deserialize rules are the same as the question "Binary Search Tree III".
+ * You must use your own data structure or your will get up to 0 point.
+ * 
+ * @Input Description
+ * In each test case, your tree should start from an empty tree.
+ * Each line will contain a operation of the tree till the end-of-file.
+ * There are two type of operations:
+ * 1. insert <data>
+ *  insert data into your tree
+ * 2. delete <data>
+ *  delete data from your tree
+ * 
+ * @Output Description
+ * The serilized string of the tree.
+ * Ending with a "\n" of each line.
+ */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class AVL_Tree {
+
+};
+
+
+int main() {
+  return 0;
+}
---
+++ Year3/S1/DS_TA/W7/q3/main.cpp
@@ -0,0 +1,36 @@
+/**
+ * Red-Black Tree
+ * 
+ * @Description:
+ * Please finish the class "Red_Black_Tree" and main function to fulfill the requirements.
+ * You can choose to implement your own Red_Black_Tree Tree or inherit from your "BinarySearchTree" or "binarySearchTree".
+ * You can add any function or variable if you want.
+ * Serialize and deserialize rules are the same as the question "Binary Search Tree III".
+ * You must use your own data structure or your will get up to 0 point.
+ * 
+ * @Input Description
+ * In each test case, your tree should start from an empty tree.
+ * Each line will contain a operation of the tree till the end-of-file.
+ * There are two type of operations:
+ * 1. insert <data>
+ *  insert data into your tree
+ * 2. delete <data>
+ *  delete data from your tree
+ * 
+ * @Output Description
+ * The serilized string of the tree.
+ * Ending with a "\n" of each line.
+ */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Red_Black_Tree {
+
+};
+
+
+int main() {
+  return 0;
+}
