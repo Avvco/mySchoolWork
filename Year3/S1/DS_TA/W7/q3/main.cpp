e6dbbed8b9c04eb26638d41fc37c3da956037e1d
Avvco <leei910110@gmail.com>
2022-09-01 17:08:18 +0800

    correct description

--- Year3/S1/DS_TA/W7/q3/main.cpp
+++ Year3/S1/DS_TA/W7/q3/main.cpp
@@ -6,19 +6,19 @@
  * You can choose to implement your own Red_Black_Tree Tree or inherit from your "BinarySearchTree" or "binarySearchTree".
  * You can add any function or variable if you want.
  * Serialize and deserialize rules are the same as the question "Binary Search Tree III".
- * You must use your own data structure or your will get up to 0 point.
+ * You must use your own data structure or you will get up to 0 points.
  * 
  * @Input Description
  * In each test case, your tree should start from an empty tree.
- * Each line will contain a operation of the tree till the end-of-file.
- * There are two type of operations:
+ * Each line will contain an operation of the tree till the end-of-file.
+ * There are two types of operations:
  * 1. insert <data>
  *  insert data into your tree
  * 2. delete <data>
  *  delete data from your tree
  * 
  * @Output Description
- * The serilized string of the tree.
+ * The serialized string of the tree.
  * Ending with a "\n" of each line.
  */
 
@@ -29,7 +29,6 @@ using namespace std;
 class Red_Black_Tree {
 
 };
-
 
 int main() {
   return 0;
